9c3efcf3519eff0260e117e79291b34497473284
Alexander Kim <limosha@inbox.ru>
2025-09-28 15:49:17 +0300

    tests: implement Task-3 test via console program

---
+++ src/lab-1/Task3/Console/Program.cs
@@ -0,0 +1,40 @@
+using Task3.Entities;
+using Task3.Implementations;
+
+namespace Task3.Console;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        const int channelsVolume = 3;
+        const int batchVolume = 5;
+        var batchTimeout = TimeSpan.FromMilliseconds(100);
+        const int totalMessages = 1000;
+
+        var messageHandler = new MessageHandler();
+        var messageProcessor = new MessageProcessor(channelsVolume, messageHandler, batchVolume, batchTimeout);
+
+        MessageProcessor processor = messageProcessor;
+        MessageProcessor sender = messageProcessor;
+
+        ArgumentNullException.ThrowIfNull(processor);
+        ArgumentNullException.ThrowIfNull(sender);
+
+        var cts = new CancellationTokenSource();
+        Task processingTask = processor.ProcessAsync(cts.Token);
+
+        IEnumerable<Message> messages = Enumerable.Range(1, totalMessages)
+            .Select(n => new Message($"#{n}", "text"));
+
+        await Parallel.ForEachAsync(
+                messages,
+                cts.Token,
+                async (message, token) => await sender.SendAsync(message, token))
+            .ConfigureAwait(false);
+
+        processor.Complete();
+        cts.Dispose();
+        await processingTask.ConfigureAwait(false);
+    }
+}
