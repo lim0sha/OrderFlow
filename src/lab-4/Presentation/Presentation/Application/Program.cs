5f37f7f3b342b8279f1cc1a8a323bd57fa22c400
Alexander Kim <limosha@inbox.ru>
2025-12-08 00:29:57 +0300

    feat [lab-4]: implement entry point

--- src/lab-4/Presentation/Presentation/Application/Program.cs
+++ src/lab-4/Presentation/Presentation/Application/Program.cs
@@ -1,8 +1,16 @@
+using Confluent.Kafka;
 using DataAccess.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Orders.Kafka.Contracts;
 using Presentation.Extensions;
+using Presentation.Kafka.Abstractions.Extensions;
+using Presentation.Kafka.Abstractions.Options;
+using Presentation.Kafka.Serializers;
 using Presentation.Services;
 using Task1.Clients.Extensions;
 using Task2.Extensions;
@@ -11,13 +19,17 @@ namespace Presentation.Application;
 
 internal abstract class Program
 {
+#pragma warning disable CA1506
     private static Task Main(string[] args)
+#pragma warning restore CA1506
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         builder.Configuration
             .SetBasePath(AppContext.BaseDirectory)
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
         Console.WriteLine($"[DEBUG] Current directory: {Directory.GetCurrentDirectory()}");
         Console.WriteLine($"[DEBUG] AppContext.BaseDirectory: {AppContext.BaseDirectory}");
@@ -38,6 +50,24 @@ internal abstract class Program
             .AddRepositories()
             .AddApplicationServices()
             .AddMigrationsFromOptions();
+
+        builder.Services.AddSingleton(Serializers.Int64);
+        builder.Services
+            .AddSingleton<ISerializer<OrderCreationValue>, ProtobufSerializer<OrderCreationValue>>();
+
+        builder.Services.AddKafkaProducer(
+            topic: "order_creation",
+            keySerializer: builder.Services.BuildServiceProvider().GetRequiredService<ISerializer<long>>(),
+            valueSerializer: builder.Services.BuildServiceProvider()
+                .GetRequiredService<ISerializer<OrderCreationValue>>());
+
+        builder.Services.Configure<KafkaOptions>(builder.Configuration.GetSection("Kafka"));
+
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(8080, o => o.Protocols = HttpProtocols.Http1);
+            options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http2);
+        });
 
         WebApplication app = builder.Build();
 
